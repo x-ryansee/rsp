a5180a76378d73faf0ae0e7e51dd3bcceaaf072a
Ryan See <ryanjosephsee@gmail.com>
2024-01-31 21:22:42 -0800

    animated h1s

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useState,useRef, useEffect } from 'react';
 import { Canvas, useFrame, useLoader } from '@react-three/fiber';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { AnimationMixer } from 'three';
@@ -22,15 +22,34 @@ const Model = () => {
 };
 
 const HomePage = () => {
-  return (
-    <Canvas className="model-canvas" data-aos="fade-up" data-aos-offset="200" data-aos-once="true"
-            camera={{ position: [-5, 2, 5], fov: 50 }}>
-      <ambientLight intensity={0.5} />
-      <spotLight position={[10, 10, 10]} angle={.15} penumbra={1} />
-      <pointLight position={[0, 0, 0]} />
-      <Model />
-    </Canvas>
-  );
-};
-
-export default HomePage;
+    const [fade, setFade] = useState(1); // Opacity starts at full
+    const ref = useRef();
+  
+    useEffect(() => {
+      const handleScroll = () => {
+        const position = window.pageYOffset;
+        // Assuming 1000 is where you want to start fading out
+        const opacity = Math.max(1 - position / 1000, 0);
+        setFade(opacity);
+      };
+  
+      window.addEventListener('scroll', handleScroll, { passive: true });
+  
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
+    }, []);
+  
+    return (
+      <div ref={ref} style={{ opacity: fade }}>
+        <Canvas className="model-canvas" camera={{ position: [-5, 2, 5], fov: 50 }}>
+            <ambientLight intensity={0.5} />
+            <spotLight position={[10, 10, 10]} angle={.15} penumbra={1} />
+            <pointLight position={[0, 0, 0]} />
+            <Model />
+        </Canvas>
+      </div>
+    );
+  };
+  
+  export default HomePage;
